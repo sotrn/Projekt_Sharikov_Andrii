1490ca0d7d812e033ae41d4385ba86bb1be4e605
Andrey Sharikov <sarikovandrej139@gmail.com>
2026-03-23 12:25:59 +0100

    hlavni menu

--- Projekt_Sharikov_Andrii/Hlav.cs
+++ Projekt_Sharikov_Andrii/Hlav.cs
@@ -19,7 +19,83 @@ namespace Projekt_Sharikov_Andrii
 
         private void InitializeComponent()
         {
-
+            ComponentResourceManager resources = new ComponentResourceManager(typeof(Form2));
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            ((ISupportInitialize)pictureBox1).BeginInit();
+            ((ISupportInitialize)pictureBox2).BeginInit();
+            ((ISupportInitialize)pictureBox3).BeginInit();
+            ((ISupportInitialize)pictureBox4).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(72, 644);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 87);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox2.Location = new Point(336, 644);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 87);
+            pictureBox2.TabIndex = 1;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(656, 644);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 87);
+            pictureBox3.TabIndex = 2;
+            pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.Location = new Point(113, 54);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(577, 549);
+            pictureBox4.TabIndex = 3;
+            pictureBox4.TabStop = false;
+            // 
+            // Form2
+            // 
+            AutoSize = true;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(800, 800);
+            Controls.Add(pictureBox4);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            ((ISupportInitialize)pictureBox1).EndInit();
+            ((ISupportInitialize)pictureBox2).EndInit();
+            ((ISupportInitialize)pictureBox3).EndInit();
+            ((ISupportInitialize)pictureBox4).EndInit();
+            ResumeLayout(false);
         }
+
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox4;
+        private PictureBox pictureBox3;
     }
+
 }
+
