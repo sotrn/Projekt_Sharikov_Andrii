de465fac4521fa5549f05c59e5ad85386fb9ee80
Andrey Sharikov <sarikovandrej139@gmail.com>
2026-04-01 11:41:50 +0200

    vyřešil problém s tím že se nespoštělo Hlavni Menu

---
+++ Projekt_Sharikov_Andrii/HlavniMenu.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Projekt_Sharikov_Andrii
+{
+    partial class HlavniMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HlavniMenu));
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(57, 123);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(334, 340);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // HlavniMenu
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            BackgroundImageLayout = ImageLayout.Zoom;
+            ClientSize = new Size(446, 448);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "HlavniMenu";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "HlavniMenu";
+            WindowState = FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+    }
+}
--- Projekt_Sharikov_Andrii/Load.cs
+++ Projekt_Sharikov_Andrii/Load.cs
@@ -15,6 +15,12 @@ namespace Projekt_Sharikov_Andrii
         private void timer1_Tick(object sender, EventArgs e)
         {
             progressBar1.Value = progressBar1.Value + 1 % 100;
+                timer1.Stop();
+                HlavniMenu form1 = new HlavniMenu();
+                form1.Show();
+                this.Hide();
+            
         }
+
     }
 }
